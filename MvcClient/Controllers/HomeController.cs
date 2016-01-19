6c0bc5d46e93771c846393f4000f61023fd59e0d
goldenKeyHu <2410753379@qq.com>
2016-01-19 14:02:24 +0800

    作为标准的WebClient来发展

---
+++ MvcClient/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
